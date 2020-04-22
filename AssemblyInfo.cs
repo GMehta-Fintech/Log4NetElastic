d5ba0e002a23801cf04451ab63104a14e64100cc
Gaurav Mehta <38317292+GMehta-Fintech@users.noreply.github.com>
2020-04-22 15:21:21 +0530

    Test App for log4net Elastic Search and file log

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+//
+// C# yada yada yada
+//
+
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "app.config", Watch = true)]
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using log4net;
+using log4net.Repository.Hierarchy;
+using log4net.Core;
+using log4net.Appender;
+using log4net.Layout;
+
+namespace TestConsol
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+
+            log.Info("Hello Info world!");
+            log.Error("Hello Error world!");
+            log.Warn("Hello Warn world!");
+
+            log.Info("Hello Info world!");
+            log.Error("Hello Error world!");
+            log.Warn("Hello Warn world!");
+
+            log.Info("Hello Info world!");
+            log.Error("Hello Error world!");
+            log.Warn("Hello Warn world!");
+
+            Console.WriteLine("Hit enter");
+            Console.ReadLine();
+
+        }
+    }
+}
